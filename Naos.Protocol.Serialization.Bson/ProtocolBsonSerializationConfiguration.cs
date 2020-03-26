dd3a872e347c77a1a71fbb78e8ced3eb87e03423
Lawson Caudill <lawson@getthinktank.com>
2020-03-26 16:22:50 +0000

    Added Bson and Json config projects.

---
+++ Naos.Protocol.Serialization.Bson/ProtocolBsonSerializationConfiguration.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ProtocolBsonSerializationConfiguration.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Protocol.Serialization.Bson
+{
+    using System;
+    using System.Collections.Generic;
+    using Naos.Protocol.Domain;
+    using OBeautifulCode.Serialization.Bson;
+
+    /// <inheritdoc />
+    public class ProtocolBsonSerializationConfiguration : BsonConfigurationBase
+    {
+        /// <inheritdoc />
+        protected override IReadOnlyCollection<Type> TypesToAutoRegister => new Type[]
+        {
+            typeof(EventBase),
+            typeof(VoidOperationBase),
+            typeof(ReturningOperationBase<>),
+            typeof(StreamLocatorBase),
+        };
+    }
+}
---
+++ Naos.Protocol.Serialization.Json/ProtocolJsonSerializationConfiguration.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ProtocolJsonSerializationConfiguration.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Protocol.Serialization.Json
+{
+    using System;
+    using System.Collections.Generic;
+    using Naos.Protocol.Domain;
+    using OBeautifulCode.Serialization.Json;
+
+    /// <inheritdoc />
+    public class ProtocolJsonSerializationConfiguration : JsonConfigurationBase
+    {
+        /// <inheritdoc />
+        protected override IReadOnlyCollection<Type> TypesToAutoRegister => new Type[]
+        {
+            typeof(EventBase),
+            typeof(VoidOperationBase),
+            typeof(ReturningOperationBase<>),
+            typeof(StreamLocatorBase),
+        };
+    }
+}
