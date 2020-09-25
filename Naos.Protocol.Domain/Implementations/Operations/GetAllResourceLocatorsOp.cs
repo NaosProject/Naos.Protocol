f739b408f832f040cb93070a22ddf1b45514c030
Lawson Caudill <lawson@getthinktank.com>
2020-09-25 23:17:45 +0100

    Moved ResourceLocator base logic back into protocol.

---
+++ Naos.Protocol.Domain/Implementations/Operations/GetAllResourceLocatorsOp.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GetAllResourceLocatorsOp.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Protocol.Domain
+{
+    using System;
+    using System.Collections.Generic;
+    using Naos.CodeAnalysis.Recipes;
+
+    /// <summary>
+    /// Operation to a <see cref="IResourceLocator"/> by a <see cref="Type"/>.
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1702:CompoundWordsShouldBeCasedCorrectly", MessageId = "AllStream", Justification = NaosSuppressBecause.CA1702_CompoundWordsShouldBeCasedCorrectly_AnalyzerIsIncorrectlyDetectingCompoundWords)]
+    public partial class GetAllResourceLocatorsOp : ReturningOperationBase<IReadOnlyCollection<IResourceLocator>>
+    {
+    }
+}
---
+++ Naos.Protocol.Domain/Model/ResourceLocation/ResourceLocatorBase.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ResourceLocatorBase.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Protocol.Domain
+{
+    using OBeautifulCode.Type;
+
+    /// <summary>
+    /// Base class for locators of any type.
+    /// </summary>
+    public abstract partial class ResourceLocatorBase : IResourceLocator, IModelViaCodeGen
+    {
+    }
+}
