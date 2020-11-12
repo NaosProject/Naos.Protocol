40efd0a9486746baa560241b212cd7dca08670cd
Lawson Caudill <lawson@getthinktank.com>
2020-11-12 18:31:59 +0000

    Changed IProtocolResourceLocator to support multiple ID types with the same interface, updated SingleLocator accordingly.

--- Naos.Protocol.Domain/Implementations/ResourceLocation/SingleResourceLocatorProtocol.cs
+++ Naos.Protocol.Domain/Implementations/ResourceLocation/SingleResourceLocatorProtocol.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="SingleResourceLocatorProtocol{TId}.cs" company="Naos Project">
+// <copyright file="SingleResourceLocatorProtocol.cs" company="Naos Project">
 //    Copyright (c) Naos Project 2019. All rights reserved.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
@@ -11,35 +11,20 @@ namespace Naos.Protocol.Domain
     using System.Threading.Tasks;
 
     /// <summary>
-    /// Implements <see cref="IProtocolResourceLocator{TId}"/> using a single provided <see cref="ResourceLocatorBase"/>.
+    /// Implements <see cref="IProtocolResourceLocator"/> using a single provided <see cref="ResourceLocatorBase"/>.
     /// </summary>
-    /// <typeparam name="TId">Type of ID.</typeparam>
-    public sealed partial class SingleResourceLocatorProtocol<TId>
-        : IProtocolResourceLocator<TId>
+    public sealed partial class SingleResourceLocatorProtocol
+        : IProtocolResourceLocator
     {
         private readonly IResourceLocator resourceLocator;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="SingleResourceLocatorProtocol{TId}"/> class.
+        /// Initializes a new instance of the <see cref="SingleResourceLocatorProtocol"/> class.
         /// </summary>
         /// <param name="resourceLocator">The SQL stream locator.</param>
         public SingleResourceLocatorProtocol(IResourceLocator resourceLocator)
         {
             this.resourceLocator = resourceLocator ?? throw new ArgumentNullException(nameof(resourceLocator));
-        }
-
-        /// <inheritdoc />
-        public IResourceLocator Execute(
-            GetResourceLocatorByIdOp<TId> operation)
-        {
-            return this.resourceLocator;
-        }
-
-        /// <inheritdoc />
-        public async Task<IResourceLocator> ExecuteAsync(
-            GetResourceLocatorByIdOp<TId> operation)
-        {
-            return await Task.FromResult(this.Execute(operation));
         }
 
         /// <inheritdoc />
@@ -57,6 +42,12 @@ namespace Naos.Protocol.Domain
             GetAllResourceLocatorsOp operation)
         {
             return await Task.FromResult(this.Execute(operation));
+        }
+
+        /// <inheritdoc />
+        public ISyncAndAsyncReturningProtocol<GetResourceLocatorByIdOp<TId>, IResourceLocator> GetResourceLocatorByIdProtocol<TId>()
+        {
+            return new LambdaReturningProtocol<GetResourceLocatorByIdOp<TId>, IResourceLocator>(_ => this.resourceLocator);
         }
     }
 }
---
+++ Naos.Protocol.Domain/Model/ResourceLocation/IProtocolResourceLocator.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IProtocolResourceLocator.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Protocol.Domain
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Set of common protocols around <see cref="IResourceLocator" /> for use with protocols accessing communication or storage.
+    /// </summary>
+    public interface IProtocolResourceLocator
+        : ISyncAndAsyncReturningProtocol<GetAllResourceLocatorsOp, IReadOnlyCollection<IResourceLocator>>
+    {
+        /// <summary>
+        /// Gets the resource locator by identifier protocol.
+        /// </summary>
+        /// <typeparam name="TId">The type of the identifier.</typeparam>
+        /// <returns>Protocol for <see cref="GetResourceLocatorByIdProtocol{TId}"/>.</returns>
+        ISyncAndAsyncReturningProtocol<GetResourceLocatorByIdOp<TId>, IResourceLocator> GetResourceLocatorByIdProtocol<TId>();
+    }
+}
